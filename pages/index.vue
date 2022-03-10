3b64a008b3105b4411b8215b2cca6d131b51ee32
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-03-10 17:45:31 +0100

    first deploy to firebase

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="text-red-400 bg-slate-400"> Hello</div>
+    <div class="min-h-screen w-full flex items-center justify-center">
+        <div class="text-center">
+            <img class="w-36 m-auto" src="/aquaterm-logo.png" />
+            <h2 class="mt-4 text-3xl ">AquaTerm Grup SRL</h2>
+            <h3 class="mt-4 text-lg text-blue-800">062053053</h3>
+        </div>
+
+    </div>
 </template>
 
 <script setup>

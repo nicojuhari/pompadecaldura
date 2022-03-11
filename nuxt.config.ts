bc137db89fb2c59ca501b55a29fe6eb032755fc5
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-03-11 14:47:37 +0100

    removed firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     buildModules: ['@nuxtjs/tailwindcss'],
     routes: {
     '/': { static: true }, // Once per build (via builder)
-    // '/blog/*': { static: true }, // Once on-demand per build (via lambda)
+    '/contacts': { ssr: false }, // Once on-demand per build (via lambda)
     // '/stats/*': { swr: '10 min' }, // Once on-demand each 10 minutes (via lambda)
     // '/admin/*': { ssr: false }, // Client-Side rendered
     // '/react/*': { redirect: '/vue' }, // Redirect Rules
---
+++ pages/contacts.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="min-h-screen w-full flex items-center justify-center">
+        <div class="text-center">
+            <div>Contacts</div>
+            <h2 class="mt-4 text-3xl ">AquaTerm Grup SRL</h2>
+            <h3 class="mt-4 text-lg text-blue-800">062053053</h3>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>

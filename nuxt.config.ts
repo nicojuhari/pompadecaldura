777711bc6ebab7fa04d39b03c90cbb9944c24b63
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-03-11 22:41:31 +0100

    trying static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: ['@nuxtjs/tailwindcss'],
-    //ssr: false,
+    ssr: false,
     routes: {
       '/': { static: true }, // Once per build (via builder)
       // '/contacts': { ssr: false }, // Once on-demand per build (via lambda)

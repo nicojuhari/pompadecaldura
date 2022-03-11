f9936164403ea477d910f0a1f5a73ada72f6545a
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-03-11 22:46:06 +0100

    preset browser

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,15 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: ['@nuxtjs/tailwindcss'],
-    ssr: false,
-    routes: {
-      '/': { static: true }, // Once per build (via builder)
-      // '/contacts': { ssr: false }, // Once on-demand per build (via lambda)
-      // // '/stats/*': { swr: '10 min' }, // Once on-demand each 10 minutes (via lambda)
-      // // '/admin/*': { ssr: false }, // Client-Side rendered
-      // // '/react/*': { redirect: '/vue' }, // Redirect Rules
+    nitro: {
+      preset: 'browser',
     }
+    // ssr: false,
+    // routes: {
+    //   '/': { static: true }, // Once per build (via builder)
+    //   // '/contacts': { ssr: false }, // Once on-demand per build (via lambda)
+    //   // // '/stats/*': { swr: '10 min' }, // Once on-demand each 10 minutes (via lambda)
+    //   // // '/admin/*': { ssr: false }, // Client-Side rendered
+    //   // // '/react/*': { redirect: '/vue' }, // Redirect Rules
+    // }
 })

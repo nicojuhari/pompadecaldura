ec983e1e8cbb681cce117e3059cd860de8c143c9
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-03-11 22:08:46 +0100

    preset browser

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: ['@nuxtjs/tailwindcss'],
-    ssr: false,
+    nitro: {
+    preset: 'browser',
+  }
+    //ssr: false,
     // routes: {
     // '/': { static: true }, // Once per build (via builder)
     // '/contacts': { ssr: false }, // Once on-demand per build (via lambda)

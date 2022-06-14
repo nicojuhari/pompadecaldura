e6cc62e8e6674a28fb725bc0f7e4edcc7db9f88c
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-06-14 22:38:20 +0200

    try to deploy SSG to netlify

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -6,12 +6,12 @@
         <div class="max-w-screen-xl w-full m-auto">
             <div class="text-center text-2xl md:text-7xl leading-tight">
                 <h1>Pompa de caldură <em class="font-bold uppercase">Q-Term</em></h1>
-                <div class="md:text-4xl mt-4 md:mt-6"><em>Încălzire, Apă caldă și Răcirea locuinței</em></div>
+                <div class="md:text-4xl mt-4 md:mt-6 opacity-70"><em>Încălzire, Apă caldă și Răcirea locuinței</em></div>
             </div>
             <div class="mt-8">
                 <img class="m-auto w-60 md:w-80" src="/images/q-term-hero-premium.png" alt="pompa de caldură Q-Term">
                 <div class="text-2xl md:text-4xl text-center mt-4">
-                    <span class="text-brand font-bold ">Mai ieftin,</span> decât cu GAZ, cărbune, sau lemne 🔥
+                    <span class="text-brand font-bold ">Mai ieftin de 3 ori,</span> decât cu GAZ, cărbune, sau lemne 🔥
                 </div>
             </div>
         </div>

b11f08ac287d5738158875472a43aa3318cf33b5
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-07-27 11:45:54 +0300

    adds brand colors

--- components/FeaturesSection.vue
+++ components/FeaturesSection.vue
@@ -2,28 +2,28 @@
     let list = [
         {
             icon: 'material-symbols:workspace-premium-rounded',
-            color: 'brand',
+            color: 'text-brand',
             text: 'Calitate premium și ultra design',
         },
         {
             icon: 'prime:volume-down',
-            color: 'blue-600',
+            color: 'text-brand-blue',
             text: 'Funcționare silențioasă, sub 50 dB',
         },
          {
             icon: 'material-symbols:eco-outline',
-            color: 'green-600',
+            color: 'text-brand-green',
             text: 'Ecologic, cu 70% emisii mai scăzute de CO₂',
         },
             {
             icon: 'system-uicons:lightning',
-            color: 'yellow-600',
+            color: 'text-brand-yellow',
             text: 'Evaluarea energică europeană A+++',
         },
     ]
 </script>   
 <template>
-    <section v-once>
+    <section>
         <div class="container">
             <div class="section-title">Beneficii</div>
             <div class="grid md:grid-cols-2 gap-8">
@@ -33,20 +33,12 @@
                 </div>
                 <div class="grid place-content-center gap-6">
                     <div class="flex gap-4 items-center justify-left" v-for="item in list">
-                        <div :class="' text-' + item.color" class="p-2 bg-opacity-20 rounded-full">
+                        <div :class="item.color">
                             <Icon :name="item.icon" class="h-12 w-12" />
                         </div>
                         <div class="text-2xl">{{ item.text }}</div>
                     </div>
                 </div>
-            </div>
-            <div class="grid md:grid-cols-2 text-center gap-6 gap-y-16 text-xl">
-
-
-                
-
-
-    
             </div>
         </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,10 @@ module.exports = {
         extend: {
             colors: {
                 brand: "#dd231c", //red
-                color2: "#151e3f", //dark-blue
-                color3: "#2274a5",
-                color4: "#009b72",
+                "brand-blue": "#3F88C5", //darker-blue
+                "brand-dark-blue": "#151e3f", //dark-blue
+                "brand-green": "#17A398", //green
+                "brand-yellow": "#FFB627", //yellow
             },
         },
     },
